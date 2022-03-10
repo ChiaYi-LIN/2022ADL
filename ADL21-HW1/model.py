4933e1d855cc5013f8298f16450b42c8c799a566
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-03-10 14:19:01 +0800

    add lstm

--- ADL21-HW1/model.py
+++ ADL21-HW1/model.py
@@ -25,6 +25,13 @@ class SeqClassifier(torch.nn.Module):
                             batch_first=True,
                             dropout=dropout,
                             bidirectional=True)
+
+        self.lstm = nn.LSTM(input_size=embeddings.shape[1],
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True,
+                            dropout=dropout,
+                            bidirectional=True)
         
         self.fc = nn.Linear(2 * hidden_size, num_class)
         self.act = nn.Softmax(dim=1)
@@ -37,16 +44,17 @@ class SeqClassifier(torch.nn.Module):
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
         # raise NotImplementedError
-        embeds = self.embed(batch)
-        out, h = self.gru(embeds)
-
-        #concat the final forward and backward hidden state
-        hidden = torch.cat((h[-2,:,:], h[-1,:,:]), dim = 1)
-        #hidden = [batch size, hid dim * num directions]
+        embeds = self.embed(batch) # embeds = torch.Size([batch size, seq length, embeddings])
         
-        dense_outputs = self.fc(hidden)
+        out, h = self.gru(embeds) # out = torch.Size([batch size, seq length, 2 * hid dim]), h = torch.Size([2 * num layers, batch size, hid dim])
+        # out, (h, c) = self.lstm(embeds)
+
+        # concat the final forward and backward hidden state
+        hidden = torch.cat((h[-2,:,:], h[-1,:,:]), dim = 1) # hidden = torch.Size([batch size, 2 * hid dim])
+        
+        dense_outputs = self.fc(hidden) # dense_outputs = torch.Size([batch size, num class])
         
         #Final activation function
-        outputs = self.act(dense_outputs)
+        outputs = self.act(dense_outputs) # outputs = torch.Size([batch size, num class])
         
         return outputs
