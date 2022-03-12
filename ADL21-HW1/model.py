fix bug (should not use softmax)

ADL21-HW1/model.py
@@ -35,7 +35,6 @@ class SeqClassifier(torch.nn.Module):
                             bidirectional=True)
         
         self.fc = nn.Linear(2 * hidden_size, num_class)
-        self.act = nn.Softmax(dim=1)
 
     # @property
     # def encoder_output_size(self) -> int:
@@ -64,11 +63,7 @@ class SeqClassifier(torch.nn.Module):
         hidden = torch.cat((h[-2,:,:], h[-1,:,:]), dim = 1) # hidden = torch.Size([batch size, 2 * hid dim])
         # print('hidden =', hidden.shape)
 
-        dense_outputs = self.fc(hidden) # dense_outputs = torch.Size([batch size, num class])
-        # print('dense_outputs =', dense_outputs.shape)
-
-        #Final activation function
-        outputs = self.act(dense_outputs) # outputs = torch.Size([batch size, num class])
+        outputs = self.fc(hidden) # dense_outputs = torch.Size([batch size, num class])
         # print('outputs =', outputs.shape)
 
         return outputs
@@ -85,6 +80,7 @@ class SeqIOBTagger(torch.nn.Module):
     ) -> None:
         super(SeqIOBTagger, self).__init__()
         self.num_class = num_class
+        self.bidirectional = bidirectional
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
         # TODO: model architecture
         self.gru = nn.GRU(input_size=embeddings.shape[1],
@@ -92,17 +88,19 @@ class SeqIOBTagger(torch.nn.Module):
                             num_layers=num_layers,
                             batch_first=True,
                             dropout=dropout,
-                            bidirectional=True)
+                            bidirectional=bidirectional)
 
         self.lstm = nn.LSTM(input_size=embeddings.shape[1],
                             hidden_size=hidden_size,
                             num_layers=num_layers,
                             batch_first=True,
                             dropout=dropout,
-                            bidirectional=True)
+                            bidirectional=bidirectional)
         
-        self.fc = nn.Linear(2 * hidden_size, num_class)
-        self.act = nn.Softmax(dim=2)
+        if self.bidirectional:
+            self.fc = nn.Linear(2 * hidden_size, num_class)
+        else:
+            self.fc = nn.Linear(hidden_size, num_class)
 
     # @property
     # def encoder_output_size(self) -> int:
@@ -121,17 +119,14 @@ class SeqIOBTagger(torch.nn.Module):
         # out, h = self.gru(embeds) # out = torch.Size([batch size, seq length, 2 * hid dim]), h = torch.Size([2 * num layers, batch size, hid dim])
         # out, (h, c) = self.lstm(embeds)
         
+        # packed_out, h = self.gru(pack_embeds)
         packed_out, (h, c) = self.lstm(pack_embeds)
 
         out, _ = rnn.pad_packed_sequence(packed_out, batch_first=True)
 
         tag_space = self.fc(out) # tag_space = torch.Size([batch size, seq length, num_class])
         # print('tag_space = ', tag_space.shape)
-
-        outputs = self.act(tag_space) # outputs = torch.Size([batch size, seq length, num class])
-        # print('outputs = ', outputs.shape)
-        # print(outputs)
         
-        outputs = outputs.transpose(-1, 1) # outputs = torch.Size([batch size, num_class, seq length])
+        outputs = tag_space.transpose(-1, 1) # outputs = torch.Size([batch size, num_class, seq length])
         
         return outputs