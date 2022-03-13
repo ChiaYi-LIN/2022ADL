132925e511221adda26e230cd72397a511d40125
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-03-13 11:55:48 +0800

    change export format

--- ADL21-HW1/export.sh
+++ ADL21-HW1/export.sh
@@ -1,1 +1,15 @@
-zip adl_hw1_r10922124.zip Makefile environment.yml requirements.in requirements.txt ./cache/intent/* ./cache/slot/* ./data/intent/* ./data/slot/* utils.py dataset.py model.py train_intent.py test_intent.py ./ckpt/intent/model.ckpt intent_cls.sh train_slot.py test_slot.py ./ckpt/slot/model.ckpt slot_cli.sh README.md
+mkdir -p ./r10922124
+cp ./intent_cls.sh ./r10922124/
+cp ./slot_tag.sh ./r10922124/
+cp ./README.md ./r10922124/
+# cp ./report.pdf ./r10922124/
+# cp ./download.sh ./r10922124/
+cp ./utils.py ./r10922124/
+cp ./dataset.py ./r10922124/
+cp ./model.py ./r10922124/
+cp ./train_intent.py ./r10922124/
+cp ./test_intent.py ./r10922124/
+cp ./train_slot.py ./r10922124/
+cp ./test_slot.py ./r10922124/
+
+zip adl_hw1_r10922124.zip ./cache/intent/* ./cache/slot/* ./data/intent/* ./data/slot/* ./ckpt/intent/model.ckpt ./ckpt/slot/model.ckpt
